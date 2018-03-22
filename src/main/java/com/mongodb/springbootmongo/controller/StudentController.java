11559d9e814104fc83cf03f451ef166637c24c13
WillyLu326 <luzh0326@gmail.com>
2018-03-21 21:34:07 -0400

    import StudentService into Controller

--- src/main/java/com/mongodb/springbootmongo/controller/StudentController.java
+++ src/main/java/com/mongodb/springbootmongo/controller/StudentController.java
@@ -1,13 +1,19 @@
 package com.mongodb.springbootmongo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mongodb.springbootmongo.service.StudentService;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
+	@Autowired
+	StudentService studentService;
+	
 	@GetMapping(path="/")
 	public String home() {
 		return "Student Home Page";
