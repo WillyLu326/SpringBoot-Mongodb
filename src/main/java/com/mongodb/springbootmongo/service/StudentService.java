2a4b9b794749abaacede8bf8c55bae3ca6a9fd10
WillyLu326 <luzh0326@gmail.com>
2018-03-21 21:53:51 -0400

    create findById service

--- src/main/java/com/mongodb/springbootmongo/service/StudentService.java
+++ src/main/java/com/mongodb/springbootmongo/service/StudentService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import com.mongodb.springbootmongo.model.Student;
@@ -23,6 +25,11 @@ public class StudentService {
 		return this.mongoOperations.findAll(Student.class);
 	}
 	
-	
+	public Student getStudentById(String id) {
+		Query query = new Query();
+		Criteria criteria = Criteria.where("id").is(id);
+		query.addCriteria(criteria);
+		return mongoOperations.findById(id, Student.class);
+	}
 	
 }
