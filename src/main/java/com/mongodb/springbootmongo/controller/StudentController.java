34ace6813918d93756b8ad11939cb0b7a9dc5649
WillyLu326 <luzh0326@gmail.com>
2018-03-21 21:30:01 -0400

    Controller and service testing

--- src/main/java/com/mongodb/springbootmongo/controller/StudentController.java
+++ src/main/java/com/mongodb/springbootmongo/controller/StudentController.java
@@ -1,5 +1,15 @@
 package com.mongodb.springbootmongo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
 
+	@GetMapping(path="/")
+	public String home() {
+		return "Student Home Page";
+	}
 }
--- src/main/java/com/mongodb/springbootmongo/service/StudentService.java
+++ src/main/java/com/mongodb/springbootmongo/service/StudentService.java
@@ -1,5 +1,23 @@
 package com.mongodb.springbootmongo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.stereotype.Service;
+
+import com.mongodb.springbootmongo.model.Student;
+
+@Service
 public class StudentService {
 
+	@Autowired
+	private MongoOperations mongoOperations;
+	
+	public void createStudent() {
+		Student student = new Student();
+		student.setName("Willy Lu");
+		student.setSchool("CPP");
+		
+		this.mongoOperations.save(student);
+	}
+	
 }
